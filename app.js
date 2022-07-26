78d4a1d635d1131cbef92e1531ca6757503a0793
Tanraj Dhillon <tanrajsd@gamil.com>
2022-07-25 23:06:43 -0600

    cors

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ mongoose.connect(
 
 app.use(logger("dev"));
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
